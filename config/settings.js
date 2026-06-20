a9e6f4a2ac98ba56067450ea74132f51712f0f60
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-20 15:34:14 +0100

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,4 +1,5 @@
 module.exports = {
-COOLDOWN: 60,
-KEEP_ALIVE: true
+  COOLDOWN_MS: 60 * 1000, // دقيقة واحدة
+  ICON_URL:
+    "https://cdn.discordapp.com/attachments/1515161056975126705/1515903883430465647/-_1.jpg"
 };
