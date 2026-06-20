67e101d72ca2128d890b3738fe8b56c120fe6f48
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-20 13:47:56 +0100

    Update radioPanel.js

--- embeds/radioPanel.js
+++ embeds/radioPanel.js
@@ -5,13 +5,13 @@ return new EmbedBuilder()
 .setColor("#FFD700")
 .setTitle("📻 Radio Control Panel")
 .setDescription(`
-🎧 اختر محطة للاستماع
+🎧 اختر محطة للاستماع المباشر
 
-📺 الجزيرة  
-🎙 البودكاست  
+📺 الجزيرة مباشر  
 📻 القرآن الكريم  
+🎙 بودكاست عشوائي  
 
 ⏳ تغيير المحطة متاح كل 60 ثانية
 `)
-.setFooter({ text: "Radio System • Live Control" });
+.setFooter({ text: "Radio System • Live Broadcast" });
 };
