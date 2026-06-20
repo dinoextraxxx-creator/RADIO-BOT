3c007487783153c6a603a4fb78aa5d3886ab2678
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-20 13:06:32 +0100

    Update stations.js

--- config/stations.js
+++ config/stations.js
@@ -1,1 +1,19 @@
+module.exports = [
+{
+id: "aljazeera",
+name: "📺 الجزيرة",
+url: ""
+},
 
+{
+id: "podcast",
+name: "🎙 البودكاست",
+url: ""
+},
+
+{
+id: "quran",
+name: "📻 القرآن",
+url: ""
+}
+];
