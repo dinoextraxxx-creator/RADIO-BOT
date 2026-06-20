0823acfa17a06c948bd11fcb669f9cd08fec337f
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-20 13:07:43 +0100

    Update stationSelect.js

--- menus/stationSelect.js
+++ menus/stationSelect.js
@@ -1,1 +1,19 @@
+const { StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder } = require("discord.js");
+const stations = require("../config/stations");
 
+module.exports = () => {
+
+const menu = new StringSelectMenuBuilder()
+.setCustomId("radio_select")
+.setPlaceholder("🎧 اختر محطة الراديو");
+
+stations.forEach(station => {
+menu.addOptions(
+new StringSelectMenuOptionBuilder()
+.setLabel(station.name)
+.setValue(station.id)
+);
+});
+
+return new ActionRowBuilder().addComponents(menu);
+};
