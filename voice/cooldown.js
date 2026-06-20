c3faa61435a71cb3d2c94d0e240cebc34eb34ad5
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-20 13:50:23 +0100

    Update cooldown.js

--- voice/cooldown.js
+++ voice/cooldown.js
@@ -1,1 +1,16 @@
+const cooldown = new Map();
 
+function checkCooldown(userId, time) {
+const last = cooldown.get(userId);
+
+if (last && Date.now() - last < time * 1000) {
+return false;
+}
+
+cooldown.set(userId, Date.now());
+return true;
+}
+
+module.exports = {
+checkCooldown
+};
