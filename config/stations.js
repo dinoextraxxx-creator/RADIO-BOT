7777057dc71766e8d3b38fc11da6d5158a0bb6d2
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-20 13:15:12 +0100

    Update stations.js

--- config/stations.js
+++ config/stations.js
@@ -1,19 +1,26 @@
 module.exports = [
 {
 id: "aljazeera",
-name: "📺 الجزيرة",
-url: ""
-},
-
-{
-id: "podcast",
-name: "🎙 البودكاست",
-url: ""
+name: "📺 الجزيرة مباشر",
+type: "youtube",
+url: "https://www.youtube.com/watch?v=gCNeDWCI0vo"
 },
 
 {
 id: "quran",
-name: "📻 القرآن",
-url: ""
+name: "📻 القرآن الكريم",
+type: "radio",
+url: "https://Quran-radio-stream-url-here"
+},
+
+{
+id: "podcast",
+name: "🎙 بودكاست عشوائي",
+type: "playlist",
+list: [
+"https://podcast-stream-1.mp3",
+"https://podcast-stream-2.mp3",
+"https://podcast-stream-3.mp3"
+]
 }
 ];
