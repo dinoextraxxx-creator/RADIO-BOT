41cfc4a88254ccd61d5b3e2c123eb68534d6eb1d
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-20 15:35:46 +0100

    Update connection.js

--- voice/connection.js
+++ voice/connection.js
@@ -1,18 +1,41 @@
-const { joinVoiceChannel } = require("@discordjs/voice");
+const {
+  joinVoiceChannel,
+  getVoiceConnection,
+  VoiceConnectionStatus,
+  entersState
+} = require("@discordjs/voice");
 
-let connection = null;
+const { VOICE_CHANNEL_ID } = require("../config/channels");
 
-function getConnection(guild, channel) {
+async function ensureConnection(client) {
+  let connection = getVoiceConnection(process.env.GUILD_ID);
 
-if (!connection) {
-connection = joinVoiceChannel({
-channelId: channel.id,
-guildId: guild.id,
-adapterCreator: guild.voiceAdapterCreator
-});
+  if (connection && connection.state.status !== VoiceConnectionStatus.Destroyed) {
+    return connection;
+  }
+
+  const channel = await client.channels.fetch(VOICE_CHANNEL_ID);
+  if (!channel) throw new Error("Voice channel not found");
+
+  connection = joinVoiceChannel({
+    channelId: VOICE_CHANNEL_ID,
+    guildId: channel.guild.id,
+    adapterCreator: channel.guild.voiceAdapterCreator
+  });
+
+  try {
+    await entersState(connection, VoiceConnectionStatus.Ready, 15000);
+  } catch (e) {
+    connection.destroy();
+    throw new Error("Failed to connect to voice channel");
+  }
+
+  return connection;
 }
 
-return connection;
+function disconnect() {
+  const connection = getVoiceConnection(process.env.GUILD_ID);
+  if (connection) connection.destroy();
 }
 
-module.exports = { getConnection };
+module.exports = { ensureConnection, disconnect };
