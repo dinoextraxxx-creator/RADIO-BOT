a2585b309a15b15be2cbefbddb898bc1f1e957bf
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-20 13:50:05 +0100

    Update connection.js

--- voice/connection.js
+++ voice/connection.js
@@ -1,1 +1,28 @@
+const { joinVoiceChannel } = require("@discordjs/voice");
 
+let connection = null;
+
+function getConnection(guild, channel) {
+
+if (!connection) {
+connection = joinVoiceChannel({
+channelId: channel.id,
+guildId: guild.id,
+adapterCreator: guild.voiceAdapterCreator
+});
+}
+
+return connection;
+}
+
+function destroyConnection() {
+if (connection) {
+try { connection.destroy(); } catch {}
+connection = null;
+}
+}
+
+module.exports = {
+getConnection,
+destroyConnection
+};
